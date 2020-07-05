02fb7584c2f3028697d1e739b7763b0f4e12d837
illia_pelypenko <pelypenkoillya@gmail.com>
2020-07-05 19:48:20 +0300

    move pic to s3 & add favicon

--- src/Flowers/Flower.js
+++ src/Flowers/Flower.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import serverURL from '../serverURL';
 
-const Flower = ({ flower: { _id, fileName, name, price } }) => {
-	return (
+const Flower = ({ flower: { _id, fileName, location, name, price } }) => {
+  return (
     <div className='flower'>
       <div
         className='flower__image'
         style={{
           backgroundImage: `url(
-            ${serverURL}/api/items/picture/${_id}
+            ${location}
           )`,
         }}
       ></div>
