49100f9eb16e6f855c0282964c3cef7a7984dd5c
IlliaPelypenko <35105041+illiapelypenko@users.noreply.github.com>
2020-07-09 17:33:23 +0300

    some changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import bigAbstractRose from '../../assets/big-abstract-rose.png';
+import bigAbstractRose from '../../assets/big-abstract-rose-side.png';
 
 const Header = ({ contacts }) => {
-  return (
-    <div className='header'>
-      <img
-        className='header__pic'
-        src={bigAbstractRose}
-        alt='big-abstract-rose'
-      ></img>
-      <h1>FLORIST</h1>
-      <div className='header__contacts'>
-        <a href={`tel:${contacts.phone1}`}>{contacts.phone1}</a>
-        <a href={`tel:${contacts.phone2}`}>{contacts.phone2}</a>
-        <a href={`email:${contacts.email}`}>{contacts.email}</a>
-      </div>
-    </div>
-  );
+	return (
+		<div className='header'>
+			<img
+				className='header__pic'
+				src={bigAbstractRose}
+				alt='big-abstract-rose'></img>
+			<h1>FLORIST</h1>
+			<div className='header__contacts'>
+				<a href={`tel:${contacts.phone1}`}>{contacts.phone1}</a>
+				<a href={`tel:${contacts.phone2}`}>{contacts.phone2}</a>
+				<a href={`email:${contacts.email}`}>{contacts.email}</a>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
