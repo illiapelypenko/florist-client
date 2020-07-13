3a73e84754d613222c31fe2bbf72c0cc96f797a1
IlliaPelypenko <pelypenkoillya@gmail.com>
2020-07-13 23:16:51 +0300

    change url to prod

--- src/serverURL.js
+++ src/serverURL.js
@@ -1,2 +1,2 @@
-// export default 'https://florist-test.herokuapp.com';
-export default "http://localhost:5000";
+export default "https://florist-test.herokuapp.com";
+// export default "http://localhost:5000";
